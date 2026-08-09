0125accc3ef1a2953bd8042fb0abfdef553b2d08
Danielluzius <Danielluzius@mail.de>
2026-08-09 18:49:39 +0200

    fix: update API base URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000/api/',
+  apiBaseUrl: 'http://localhost:8002/api/',
   toastDuration: 2000,
   activation: {
     successDelay: 2500,
